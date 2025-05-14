52b72f74714c39c1bca24d598f97443e876e8489
deepSaanvi1327 <deepshikha.saanvi@gmail.com>
2025-05-14 13:39:31 +0530

    Update day11-BlackJack.py

--- day11-BlackJack.py
+++ day11-BlackJack.py
@@ -1,114 +1,108 @@
 import random
 import os
 from time import sleep
-l = [2,3,4,5,6,7,8,9,{'A': 11},{'K': 10},{'Q':10},{'J':10},]
-m = [2,3,4,5,6,7,8,9,{'A': 11},{'K': 10},{'Q':10},{'J':10},]
 
-#pop out the card#
-#work on ace card#
-#test the while loop#
 
-player_cards = []
-dealer_cards = []
 
-game_over:bool = False
+def deal_card():
+    """Pick out random cards """
+    l = [2,3,4,5,6,7,8,9,10,10,10,10,11]
+    card = random.choice(l)
+    return card
 
-def user_score(user_list):
-    score = sum(user_list)
-    if score > 21:
-        for card in user_list:
-            if card == 11:
-                score -= 10
+
+def calculate_scores(deck_of_cards):
+    """Takes in a list of cards """
+    score = sum(deck_of_cards)
+    if score == 21 and len(deck_of_cards) == 2:
+        return 0
+    if score > 21 and 11 in deck_of_cards:
+        score -= 10
     return score
 
-player_score = user_score(player_cards)
-dealer_score = user_score(dealer_cards)
-
-
-def draw_cards(user_cards,l):
-    card = random.choice(l)
-    x = isinstance(card,dict)
-    if x:
-          user_cards.append(list(card.values())[0])
+def is_player_winner(score1, score2):
+    """Takes 2 integer values, checks if the player(score1) won"""
+    if score1 < 21:
+        if score1 == 0:
+            print("You Won , its a Black Jack🥳")
+#            return 1
+        elif score1 == score2:
+            print("Its a Draw.🤡")
+#            return -1
+        elif score1 < score2:
+             
+                print("You Loose😭 , your opponent outpassed you")
+        else:
+            if score1 > score2 :
+                print("You Won , you outpassed your opponent🥳")
+#                return 1
+    elif score1 == 21:
+        print("You Won , its a full score🥳")
+#        return 1
     else:
-          user_cards.append(card)
-    l.remove(card)
-        
-    return user_cards
-
-def check_score(player_score,dealer_score):
-    # global player_score,dealer_score
-    player_score = user_score(player_cards)
-    dealer_score = user_score(dealer_cards)
-    if player_score == dealer_score:
-        print("Its a Draw")
-    if player_score > 21:
-        if dealer_score > 21:
-            print("Draw")
-        else :
-            print("Dealer Wins ")
-    else:
-        if dealer_score > 21:
-            print("You Win, Horray!!")
-        else :
-            if player_score > dealer_score:
-                print("You Win, Horray!!")
-            else:
-                print("Dealer Wins ")
-
-    print(f"Players Cards were: {player_cards}, Total points = {player_score}")
-    print(f"Dealers Cards were: {dealer_cards}, Total points = {dealer_score}")
-    game_over = True
-#    return -1
-
-def clr():
-    sleep(5)
-    os.system('clear')
-
-def output_scores(player_score,dealer_score):
-    # global player_score,dealer_score
-    player_score = user_score(player_cards)
-    dealer_score = user_score(dealer_cards)
-    print(f"\nPlayer has the cards: {player_cards}. Total score = {player_score} \n")
-
-    print(f"Dealer has the card: {dealer_cards[0]}.\n")
-    clr()
-    if player_score >= 21:
-        check_score(player_score,dealer_score)
-def dealers_play(dealer_score,dealer_cards):
-    # global dealer_score,dealer_cards
-    dealer_score = user_score(dealer_cards)
-    if dealer_score < 17:
-        dealer_cards = draw_cards(dealer_cards,l)
-    else:
-        return
+        if score2 <= 21:
+            print("You Loose!😭 You have outpassed the limit")
+#            return 0
+        else:
+            print("Its a Draw,🤡 both of you have exceeded the limit")
+#            return -1
+            
 
 
+user_card = []
+computer_card = []
+user_score = -1
+computer_score = -1
+is_game_over = False
 
-print("Welcome to the ...BlackJack... Game!")
 for i in range(0,2):
-    player_cards = draw_cards(player_cards,l)
-    dealer_cards = draw_cards(dealer_cards,l)
+    user_card.append(deal_card())
+    computer_card.append(deal_card())
+
+while not is_game_over:
+
+    user_score = calculate_scores(user_card)
+    computer_score = calculate_scores(computer_card)
+    print(f"\n\nYour cards : {user_card}, current score = {user_score}")
+    print(f"Dealers first card : {computer_card[0]}.")
+
+    if user_score == 0 or computer_score == 0 or user_score > 21:
+        is_game_over = True
+    else:
+        user_should_deal = input("Type 'y' to draw another card 'n' to pass  :: ")
+        if user_should_deal == "y":
+            user_card.append(deal_card())
+        else: 
+            is_game_over = True
+
+while computer_score != 0 and computer_score < 17:
+    computer_card.append(deal_card())
+    computer_score = calculate_scores(computer_card)
+
+user_score = calculate_scores(user_card)
+computer_score = calculate_scores(computer_card)
+
+is_player_winner(user_score, computer_score)
+
+
+print("\n GAME OVER")
+print(f"Dealers cards were {computer_card}, Total score = {calculate_scores(computer_card)}")
 
 
 
-while 1:
-  if game_over == True:
-      break
-  else:
-      output_scores(player_score,dealer_score)
-      if game_over == True:
-            break
-      want_card = input("Type 'y' to get a card or 'n' to pass")
-      if want_card == 'y':
-          player_cards = draw_cards(player_cards,l)
-      else:
-          dealers_play(dealer_score,dealer_cards)
-          check_score(player_score,dealer_score)
-          break
-        
 
-print("See you in the next game!")
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
